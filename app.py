2813bc680cad789d0567a3fb8ae124b5a95ed944
Joseph Pierce <joe.pierce@endpointtech.com>
2025-06-30 09:24:43 -0500

    debug add

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from auth import *  # Register authentication routes
 from search import *  # Register search routes
 from features import *  # Register chat, prompt, notification routes
 
+@app.route('/debug_session', methods=['GET'])
+@login_required
+def debug_session():
+    return jsonify({'selected_search_ids': session.get('selected_search_ids', [])})
+
 @app.route('/robots.txt')
 def robots_txt():
     return send_from_directory('static', 'robots.txt')
